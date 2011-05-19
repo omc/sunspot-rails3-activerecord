7aa04cce4fb0c1ba49431b8b39326d716987ab10
Nick Zadrozny <nick@beyondthepath.com>
2011-05-19 07:13:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunspotRails3Activerecord::Application.config.secret_token = '128b9b3f8cfb02ffd5633ff311d9e2eb280cfc283a20bc98e708dc7a68a19f2638d0cce8e3dc3ea1d96104e510b17e11c753154935128aa5f6a4323f9850c65d'
